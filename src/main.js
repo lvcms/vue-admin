338fdeeeba065d0598e7f5d9315b7cb74f65f702
bigrocs <bigrocs@qq.com>
2018-10-04 15:36:39 +0800

    初始化 vue-admin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App)
+// }).$mount('#app')
+// 引入依赖
 import Vue from 'vue'
-import App from './App.vue'
+import JsonVue from 'json-vue-core'
+// import ContainerVueIview from 'container-vue-iview'
+// import JsonVueIview from 'json-vue-iview'
 
-Vue.config.productionTip = false
+// Vue.use(ContainerVueIview)
+// Vue.use(JsonVueIview)
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+JsonVue.start()
