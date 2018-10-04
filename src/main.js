473b8a3f159d10e9b0dd86d48ebdf6d644ba833e
bigrocs <bigrocs@qq.com>
2018-10-04 15:38:04 +0800

    删除无用代码 修改

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-// import Vue from 'vue'
-// import App from './App.vue'
-
-// Vue.config.productionTip = false
-
-// new Vue({
-//   render: h => h(App)
-// }).$mount('#app')
-// 引入依赖
 import Vue from 'vue'
 import JsonVue from 'json-vue-core'
 // import ContainerVueIview from 'container-vue-iview'
