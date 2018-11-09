7a2235d6a5e80f76ef20df74090100b1ccf5d68b
bigrocs <bigrocs@qq.com>
2018-11-09 15:58:29 +0800

    修改初始化方式

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import Vue from 'vue'
 import JsonVue from 'json-vue-core'
 import ContainerVueIview from 'container-vue-iview'
 import JsonVueIview from 'json-vue-iview'
 
-Vue.use(ContainerVueIview)
-Vue.use(JsonVueIview)
+JsonVue.use(ContainerVueIview)
+JsonVue.use(JsonVueIview)
+new JsonVue({
+    el: '#app'
+})
 
-JsonVue.start()
